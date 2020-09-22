df4a2c02fd02fffd8d8e39826a058b153cda92c6
Sarah Wood <wood.sarah@outlook.com>
2020-09-22 18:52:05 +0100

    Update

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <HelloWorld msg="This is an experiment to test and understand VueJS" />
 </template>
 
 <script>
--- vue-app/src/components/HelloWorld.vue
+++ vue-app/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>TEST VUE APP</h1>
+    <p>{{ msg }}</p>
     <!-- <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
