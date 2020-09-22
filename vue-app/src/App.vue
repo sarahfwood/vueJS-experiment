b56a9f0a4c4dbe0db96638ca7bdf4f3804b8b98a
Sarah Wood <wood.sarah@outlook.com>
2020-09-22 15:59:08 +0100

    Update

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <template></template>
 <script>
 export default {
-  name: 'component-name',
+  name: 'HelloWorld',
 }
 </script>
 
--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,10 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// createApp(App).mount('#app')
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
