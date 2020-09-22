2bffde069e43578addec4733d8205f9300b5c10e
Sarah Wood <wood.sarah@outlook.com>
2020-09-22 19:04:22 +0100

    Update

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,6 +1,28 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <HelloWorld msg="This is an experiment to test and understand VueJS" />
+
+  <!-- <div id="table">
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Joe Bloggs</td>
+        <td>joebloggs@vueexperiment.com</td>
+      </tr>
+      <tr>
+        <td>Jane Doe</td>
+        <td>janedoe@vueexperiment.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div> -->
+
 </template>
 
 <script>
@@ -9,7 +31,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HelloWorld, 
   },
 };
 </script>
