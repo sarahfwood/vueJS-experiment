50fcfd95da8ae6309ed381984f87fa4b37452218
Sarah Wood <wood.sarah@outlook.com>
2020-09-25 12:13:19 +0100

    Update

--- vue-app/src/components/HelloWorld.vue
+++ vue-app/src/components/HelloWorld.vue
@@ -23,7 +23,9 @@
   </table>
 </div>
 
-<p id="date"></p>
+<div id="app">
+  Last updated: {{ new Date().getFullYear() }}
+</div>
     <!-- <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
