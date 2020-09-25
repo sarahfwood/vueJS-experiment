f3ff6cf2d648ed40381a02c9ba7c53459d16a6a6
Sarah Wood <wood.sarah@outlook.com>
2020-09-25 20:11:27 +0100

    Update

--- vue-app/src/components/EmployeeTable.vue
+++ vue-app/src/components/EmployeeTable.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="employee-table">
     <table>
+      <thead>
+        <tr>
+          <th>Employee Name</th>
+          <th>Employee Email</th>
+        </tr>
+      </thead>
       <tbody>
         <tr v-for="employee in employees" :key="employee.id">
           <td>{{ employee.name }}</td>
