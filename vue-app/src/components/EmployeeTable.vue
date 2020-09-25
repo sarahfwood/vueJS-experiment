c7ef0901ca1538737d29b9664d2874f6db027a9c
Sarah Wood <wood.sarah@outlook.com>
2020-09-25 13:27:54 +0100

    Update

---
+++ vue-app/src/components/EmployeeTable.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Thor of Asgard</td>
+          <td>thor@avengers.com</td>
+        </tr>
+        <tr>
+          <td>Captain America</td>
+          <td>cap@avengers.com</td>
+        </tr>
+        <tr>
+          <td>Tony Stark</td>
+          <td>ironman@avengers.com</td>
+        </tr>
+        <tr>
+          <td>Natasha Romanoff</td>
+          <td>blackwidow@avengers.com</td>
+        </tr>
+        <tr>
+          <td>Bruce Banner</td>
+          <td>hulk@avengers.com</td>
+        </tr>
+        <tr>
+          <td>Carol Danvers</td>
+          <td>captainmarvel@avengers.com</td>
+        </tr>
+        <tr>
+          <td>T'Challa</td>
+          <td>blackpanther@avengers.com</td>
+        </tr>
+        <tr>
+          <td>Clint Barton</td>
+          <td>hawkeye@avengers.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employee-table",
+};
+</script>
+
+<style scoped>
+</style>
