2699b2823d1e9ba01c5229320894238e735e9b89
Sarah Wood <wood.sarah@outlook.com>
2020-09-25 14:48:35 +0100

    Update

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+  
 </template>
 
 <script>
