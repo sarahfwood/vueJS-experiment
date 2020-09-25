999a92b5aa93be6b49de328564573e93e2805d51
Sarah Wood <wood.sarah@outlook.com>
2020-09-25 14:47:33 +0100

    Update

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -9,18 +9,18 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmployeeTable,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  background: #009435;
+  border: 1px sold #009435;
+}
+
+.small container {
+  max-width: 680px;
 }
 </style>
